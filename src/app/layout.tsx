800ac5c1a5e490eaddb5ae4cca9f67b49babbbe6
Manu <mandrescabral@gmail.com>
2024-06-12 13:19:58 -0300

    chore: Update layout.tsx to include Footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+import Footer from '@/components/footer'
 
 export const metadata: Metadata = {
 	title: 'GuessRank',
@@ -16,8 +15,13 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<html lang='en'>
-			<body className={inter.className}>{children}</body>
+		<html
+			lang='en'
+			suppressHydrationWarning>
+			<body>
+				{children}
+				<Footer />
+			</body>
 		</html>
 	)
 }
