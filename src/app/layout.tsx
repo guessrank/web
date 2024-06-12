6535024ceb389126ee55694e8ea9ac9335e17745
Manu <mandrescabral@gmail.com>
2024-06-12 12:25:02 -0300

    feat: add base

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+	title: 'GuessRank',
+	description:
+		'Guess the Rank of Players in Games like Valorant, Rocket League, and more!',
+}
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<html lang='en'>
+			<body className={inter.className}>{children}</body>
+		</html>
+	)
+}
