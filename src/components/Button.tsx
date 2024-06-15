49147ec0b2aac92fa43247b350f3208e641c87c6
GabrielCarames <gabrielcarames1@gmail.com>
2024-06-15 20:33:49 -0300

    feat: add both Button and LinkButton

---
+++ src/components/Button.tsx
@@ -0,0 +1,26 @@
+import { ButtonProps } from '@/types/interfaces'
+import ButtonLayout from './ButtonLayout'
+
+export default function Button({
+	children,
+	customClassName,
+	props,
+}: {
+	children: React.ReactNode
+	customClassName?: string
+	props?: ButtonProps
+}) {
+	const className = `p-4 py-2 text-center flex justify-center items-center bg-sunglow text-black rounded-lg hover:bg-amber duration-200 ${
+		customClassName ? customClassName : ''
+	}`
+
+	return (
+		<ButtonLayout
+			props={{
+				className,
+				...props,
+			}}>
+			{children}
+		</ButtonLayout>
+	)
+}
