d2dc448ea4a05608161d78c98cc8a73aa2617448
GabrielCarames <gabrielcarames1@gmail.com>
2024-06-15 19:30:22 -0300

    feat: add Ranks section

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -1,6 +1,7 @@
 import getGameById from '@/utils/getGameById'
 import { GameProps } from '@/types/interfaces'
 import { notFound } from 'next/navigation'
+import Ranks from './components/Ranks'
 
 export default async function GamePage({
 	params,
@@ -11,19 +12,24 @@ export default async function GamePage({
 }) {
 	const game = (await getGameById({ id: params.id })) as GameProps
 	if (!game) return notFound()
-	// TODO: styling
+
 	return (
-		<main className='flex-col items-center gap-4 p-24 justify-center'>
-			<h1 className='text-5xl font-bold w-full text-center'>{game?.name}</h1>
-			<div className='w-full flex justify-center'>
-				<iframe
-					src='https://www.youtube.com/embed/5JLfjnEkeI4?si=XrT-YLTscndMhe_0'
-					className=''
-					allowFullScreen
-					loading='lazy'
-					title={game?.name}
-				/>
-			</div>
+		<main className='w-full flex items-center gap-4 p-6 justify-center h-full min-h-[calc(100vh-56px)]'>
+			<section className='w-full max-w-layout flex justify-center items-center flex-col gap-10'>
+				<h1 className='text-5xl font-bold w-full text-center'>{game?.name}</h1>
+				<div className='w-full flex justify-center'>
+					<iframe
+						src='https://www.youtube.com/embed/5JLfjnEkeI4?si=XrT-YLTscndMhe_0'
+						className='w-[800px] h-[400px]'
+						allowFullScreen
+						loading='lazy'
+						title={game?.name}
+					/>
+				</div>
+				<section className='flex justify-center w-full'>
+					<Ranks ranks={game?.ranks} />
+				</section>
+			</section>
 		</main>
 	)
 }
--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -8,10 +8,24 @@ export interface CardProps {
 	gameId: string
 }
 
+export interface DivisionProps {
+	id: number
+	name: string
+	image: string
+}
+
+export interface RankProps {
+	id: number
+	name: string
+	image: string
+	divisions: DivisionProps[]
+}
+
 export interface GameProps {
 	id: string
 	name: string
 	src: string
+	ranks: RankProps[]
 }
 
 export interface LogoProps {
