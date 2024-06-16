701d2095c9d6cb86f09f45efc4ee49e6c5cfe141
Manu <mandrescabral@gmail.com>
2024-06-16 01:36:47 -0300

    refactor: update game page layout and add report video and upload clip buttons

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -25,7 +25,7 @@ export default async function GamePage({
 				Back to home
 			</LinkButton>
 			<section className='w-full max-w-layout flex justify-center items-center flex-col gap-10'>
-				<h1 className='text-5xl font-bold w-full text-center'>{game?.name}</h1>
+				<h1 className='text-4xl font-bold w-full text-center'>{game?.name}</h1>
 				<div className='w-full flex justify-center'>
 					<iframe
 						src='https://www.youtube.com/embed/5JLfjnEkeI4?si=XrT-YLTscndMhe_0'
@@ -37,6 +37,15 @@ export default async function GamePage({
 				</div>
 				<section className='flex justify-center w-full'>
 					<Ranks ranks={game?.ranks} />
+				</section>
+				<section className='flex gap-5 justify-center w-full'>
+					<Button>Report video</Button>
+					<LinkButton
+						props={{
+							href: `/game/${game.id}/upload`,
+						}}>
+						Upload a clip
+					</LinkButton>
 				</section>
 			</section>
 		</main>
