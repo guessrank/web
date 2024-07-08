6198d40cb7293c41b028f4e2138b5254f02b6e1a
GabrielCarames <gabrielcarames1@gmail.com>
2024-07-07 23:01:05 -0300

    feat(gameService): add new method

--- src/services/gameService.tsx
+++ src/services/gameService.tsx
@@ -1,9 +1,11 @@
 import { api } from '@/helpers'
 
 export const gameApi = {
-	get: async (id: string) =>
+	getClips: async (id: string) =>
 		await api
 			.get(`/api/v1/clips?gameId=${id}&approved=false`)
 			.then((res) => res.data),
+	get: async (id: string) =>
+		await api.get(`/api/v1/games?uniqueId=${id}`).then((res) => res.data),
 	getAll: async () => await api.get(`/api/v1/games`).then((res) => res.data),
 }
