af38717a1e7ce9fcb14012874e54d7eb2fa60fb1
Manu <mandrescabral@gmail.com>
2024-07-07 23:34:36 -0300

    feat: add remotePatterns for localhost domain in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ['localhost'],
+		remotePatterns: [
+			{
+				protocol: 'http',
+				hostname: 'localhost',
+			},
+		],
 	},
 }
 
