06bd2d0f51278a50c096952d8027f0797ec79655
GabrielCarames <gabrielcarames1@gmail.com>
2024-07-07 23:00:33 -0300

    remove: remove getGameById function

--- src/utils/getGameById.ts
+++
@@ -1,9 +0,0 @@
-import readJSONFile from './readJSONFile'
-import { GameProps } from '@/types/interfaces'
-
-export default async function getGameById({ id }: { id: string }) {
-	const games = (await readJSONFile({
-		url: '/public/assets/data/games.json',
-	})) as GameProps[]
-	return games.find((game) => game.id.toString() === id)
-}
