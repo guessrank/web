c34dfa0c1d763605d551633c8e1ce311cf00063e
Manu <mandrescabral@gmail.com>
2024-07-20 15:29:42 -0300

    refactor: update Section component to use type imports for props

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,9 +1,9 @@
-import { ReactNode } from 'react'
+import type { DefaultChildProps } from '@/types/props/DefaultChildProps'
 
-export default function Section({ children }: { children: ReactNode }): JSX.Element {
-    return (
-        <section className='flex justify-center flex-col items-center max-w-layout w-full gap-24'>
-            {children}
-        </section>
-    )
+export default function Section(props: DefaultChildProps): JSX.Element {
+	return (
+		<section className='flex justify-center flex-col items-center max-w-layout w-full gap-24'>
+			{props.children}
+		</section>
+	)
 }
