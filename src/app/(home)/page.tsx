bb5597d63eee8ec59aa10352212905983cc58b57
Manu <mandrescabral@gmail.com>
2024-07-20 15:29:06 -0300

    refactor: optimize home page rendering by removing unnecessary console.log statement and improving code formatting

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,28 +1,25 @@
-import Maintenance from "@/components/Maintenance";
-import Section from "@/components/Section";
-import useGame from "@/hooks/useGame"
-import GameCard from "./components/GameCard";
-import type { GameType } from "@/types/GameType";
+import Maintenance from '@/components/Maintenance'
+import Section from '@/components/Section'
+import useGame from '@/hooks/useGame'
+import GameCard from './components/GameCard'
+import type { GameType } from '@/types/GameType'
 
 export default async function Home(): Promise<JSX.Element> {
-    const { getAllGames } = useGame();
-    const { data, error } = await getAllGames();
-    console.log(error)
-    if (error)
-        return <Maintenance/>
+	const { getAllGames } = useGame()
+	const { data, error } = await getAllGames()
+	if (error) return <Maintenance />
 	return (
-		<main className='flex flex-col items-center gap-4 p-6 w-full h-full min-h-[calc(100vh-56px)]'>
-			<Section>
-                <ul className='flex justify-center flex-wrap gap-4 w-full'>
-                    {data?.map((game: GameType, index: number) => (
-                        <GameCard 
-                            key={index}
-                            uniqueId={game.uniqueId}
-                            title={game.name}
-                            source={game.imageSrc}/>
-                    ))}
-                </ul>
-            </Section>
-		</main>
+		<Section>
+			<ul className='flex justify-center flex-wrap gap-4 w-full'>
+				{data?.map((game: GameType, index: number) => (
+					<GameCard
+						key={index}
+						uniqueId={game.uniqueId}
+						title={game.name}
+						source={game.imageSrc}
+					/>
+				))}
+			</ul>
+		</Section>
 	)
 }
