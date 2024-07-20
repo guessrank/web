34a5ba3d576b8c3eae1af7338eb181a59a647c11
Manu <mandrescabral@gmail.com>
2024-07-20 15:14:54 -0300

    refactor: update tailwind.config.ts to use single quotes consistently

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      maxWidth: {
-        layout: "1280px",
-      },
-      colors: {
-        dark: '#0E1217',
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			maxWidth: {
+				layout: '1280px',
+			},
+			colors: {
+				dark: '#0E1217',
 				shark: '#1C1F26',
-      }
-    },
-  },
-  plugins: [],
-};
-export default config;
+			},
+		},
+	},
+	plugins: [],
+}
+export default config
