3a56623a43e119b69b71e237462046b8d0aae7d3
Manu <mandrescabral@gmail.com>
2024-07-20 15:07:06 -0300

    refactor: update button styles for primary and secondary buttons

---
+++ src/styles/button.styles.ts
@@ -0,0 +1,6 @@
+export const buttonStyles = {
+	primary:
+		'flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700',
+	secondary:
+		'flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200',
+}
