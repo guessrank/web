58daf85652f96b4b5bf46661ae65ca5ee8d849dc
Manu <mandrescabral@gmail.com>
2024-07-21 04:13:46 -0300

    refactor: update Section component gap to 12

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -2,7 +2,7 @@ import type { DefaultChildProps } from '@/types/props/DefaultChildProps'
 
 export default function Section(props: DefaultChildProps): JSX.Element {
 	return (
-		<section className='flex justify-center flex-col items-center max-w-layout w-full gap-24'>
+		<section className='flex justify-center flex-col items-center max-w-layout w-full gap-12'>
 			{props.children}
 		</section>
 	)
