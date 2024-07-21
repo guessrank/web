031f6e9beae1ab5ac5c221128e3cdf0d9d6c49f6
Manu <mandrescabral@gmail.com>
2024-07-21 04:12:54 -0300

    refactor: add loading component to home page

---
+++ src/app/(home)/loading.tsx
@@ -0,0 +1,10 @@
+import Section from '@/components/Section'
+import Loading from '@/components/Loading'
+
+export default function NotFound(): JSX.Element {
+	return (
+		<Section>
+			<Loading />
+		</Section>
+	)
+}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -10,7 +10,11 @@ export default async function Home(): Promise<JSX.Element> {
 	if (error) return <Maintenance />
 	return (
 		<Section>
-			<ul className='flex justify-center flex-wrap gap-4 w-full'>
+			<div className='flex flex-col items-center gap-4'>
+				<h1 className='text-4xl font-bold'>Available games</h1>
+				<p className='text-lg text-gray-500'>Please choose a game to play</p>
+			</div>
+			<ul className='flex justify-center flex-wrap gap-4'>
 				{data?.map((game: GameType, index: number) => (
 					<GameCard
 						key={index}
