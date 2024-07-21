5b3291b97030fd577be573e9657a1457f01885ad
Manu <mandrescabral@gmail.com>
2024-07-21 04:13:08 -0300

    refactor: add link to homepage in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,19 @@
+import Link from 'next/link'
 import LogoIcon from '@/components/icons/LogoIcon'
 
 export default function Header(): JSX.Element {
 	return (
 		<header className='flex flex-col gap-6 justify-center items-center p-4'>
 			<section className='flex justify-center items-center gap-4'>
-				<LogoIcon
-					width={64}
-					height={64}
-				/>
-				<h1 className='text-4xl md:text-3xl'>GuessRank.xyz</h1>
+				<Link
+					href='/'
+					className='flex items-center gap-2'>
+					<LogoIcon
+						width={64}
+						height={64}
+					/>
+					<h1 className='text-4xl md:text-3xl'>GuessRank.xyz</h1>
+				</Link>
 			</section>
 		</header>
 	)
