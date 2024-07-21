cad64789d8c01198de33bc5e9a0f26809c7ef3dc
Manu <mandrescabral@gmail.com>
2024-07-21 04:13:25 -0300

    feat: add Loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function Loading(): JSX.Element {
+	return (
+		<div className='flex gap-2'>
+			<div className='w-5 h-5 rounded-full animate-pulse bg-indigo-600'></div>
+			<div className='w-5 h-5 rounded-full animate-pulse bg-indigo-600'></div>
+			<div className='w-5 h-5 rounded-full animate-pulse bg-indigo-600'></div>
+		</div>
+	)
+}
