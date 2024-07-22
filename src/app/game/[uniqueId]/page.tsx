04a6776fabef55d318d2b69c848a0e57f4e89207
Manu <mandrescabral@gmail.com>
2024-07-21 21:50:04 -0300

    refactor: optimize game page rendering with lazy loading for images

--- src/app/game/[uniqueId]/page.tsx
+++ src/app/game/[uniqueId]/page.tsx
@@ -1,39 +1,46 @@
 import Image from 'next/image'
 import Button from '@/components/Button'
 import Section from '@/components/Section'
+import useGame from '@/hooks/useGame'
 import type { GameProps } from '@/types/props/GameProps'
+import type { Metadata } from 'next'
+import { notFound } from 'next/navigation'
 
-const getGame = async (id: string) => {
-	const response = await fetch(
-		`http://localhost:3001/api/v1/games?uniqueId=${id}`,
-	)
-	const data = await response.json()
-	return data
+export async function generateMetadata(props: GameProps): Promise<Metadata> {
+	const { getGameById } = useGame()
+	const { data: game } = await getGameById(props.params.uniqueId)
+	if (!game)
+		return {
+			title: 'Game not found',
+			description: 'Game not found',
+		}
+	return {
+		title: game.name + ' - GuessRank',
+		description: game.description,
+	}
 }
 
 export default async function Game(props: GameProps): Promise<JSX.Element> {
-	const game = await getGame(props.params.uniqueId)
+	const { getGameById } = useGame()
+	const { data: game, error } = await getGameById(props.params.uniqueId)
+	if (error || !game) return notFound()
 	return (
 		<Section>
 			<div className='flex flex-col items-center gap-4'>
 				<div className='flex flex-col items-center gap-4 text-center'>
-					<h1 className='text-3xl font-bold'>{game.body[0].name}</h1>
-					<p className='text-lg text-gray-500 w-3/4'>
-						{game.body[0].description}
-					</p>
+					<h1 className='text-3xl font-bold'>{game.name}</h1>
+					<p className='text-lg text-gray-500 w-3/4'>{game.description}</p>
 				</div>
 				<div className='flex items-center justify-center relative w-96 h-96'>
 					<Image
-						src={game.body[0].imageSrc}
-						alt={game.body[0].name}
+						src={game.imageSrc}
+						alt={game.name}
 						width={250}
 						height={250}
 					/>
 				</div>
 				<div className='flex gap-2'>
-					<Button href={`/guessing/${game.body[0].uniqueId}`}>
-						Start guessing
-					</Button>
+					<Button href={`/guessing/${game.uniqueId}`}>Start guessing</Button>
 					<Button
 						href='/'
 						type='secondary'>
