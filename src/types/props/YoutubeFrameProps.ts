08882faabff3b4c882d9bf7a92854b96713fc41b
Manu <mandrescabral@gmail.com>
2024-07-22 17:14:39 -0300

    feat: add YoutubeFrameProps interface for representing props of a YoutubeFrame component

---
+++ src/types/props/YoutubeFrameProps.ts
@@ -0,0 +1,9 @@
+export interface YoutubeFrameProps {
+	url: string
+	title: string
+	width?: string
+	height?: string
+	// onEnd: () => void
+	// onReady: () => void
+	// onStateChange: (state: any) => void
+}
