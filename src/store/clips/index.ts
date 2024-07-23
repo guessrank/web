d4ea870507ca36560e5871fb4effedd85ae613aa
Manu <mandrescabral@gmail.com>
2024-07-23 00:31:59 -0300

    refactor: update Clips store to include setClips action and get method

--- src/store/clips/index.ts
+++ src/store/clips/index.ts
@@ -1,17 +1,19 @@
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
-import { removeClip, setCurrentClip, removeAllClips } from './actions'
+import { removeClip, setCurrentClip, setClips, removeAllClips } from './actions'
 import type { ClipsBaseState } from '@/types/states/ClipsBaseState'
 
 const useClipsStore = create<ClipsBaseState>()(
 	persist(
-		(set) => ({
+		(set, get) => ({
 			clips: [],
 			currentIndex: 0,
 			current: null,
-			remove: removeClip(set),
+			remove: removeClip(set, get),
+			setClips: setClips(set),
 			setCurrent: setCurrentClip(set),
 			removeAll: removeAllClips(set),
+			get: () => get().clips,
 		}),
 		{
 			name: 'clips',
