4c5fdbf76bfa2f97c10a77fffd40e404468a1994
Manu <mandrescabral@gmail.com>
2024-07-23 00:32:14 -0300

    refactor: update removeClip action in Clips store to use slice method

--- src/store/clips/actions.ts
+++ src/store/clips/actions.ts
@@ -1,9 +1,8 @@
-export const removeClip = (set: any) => (index: number) => {
-	set((state: any) => {
-		const clips = [...state.clips]
-		clips.splice(index, 1)
-		return { clips }
-	})
+import type { ClipType } from '@/types/ClipType'
+
+export const removeClip = (set: any, get: any) => () => {
+	const newClips = get().clips.slice(1)
+	set({ clips: newClips })
 }
 
 export const setCurrentClip = (set: any) => (index: number) => {
@@ -12,5 +11,7 @@ export const setCurrentClip = (set: any) => (index: number) => {
 		return { currentClip, currentClipIndex: index }
 	})
 }
+
+export const setClips = (set: any) => (clips: ClipType[]) => set({ clips })
 
 export const removeAllClips = (set: any) => () => set({ clips: [] })
