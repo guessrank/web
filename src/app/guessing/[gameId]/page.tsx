e47acac01585dc9c67ecc5bf37091db2a5b5a38f
Manu <mandrescabral@gmail.com>
2024-07-23 00:30:09 -0300

    refactor: update Guessing page to use GuessingHeader component and remove unused code

--- src/app/guessing/[gameId]/page.tsx
+++ src/app/guessing/[gameId]/page.tsx
@@ -1,29 +1,22 @@
 import Section from '@/components/Section'
-import YoutubeFrame from './components/YoutubeFrame'
-import useClip from '@/hooks/useClip'
 import Invalid from './components/Invalid'
+import GuessingHeader from './components/GuessingHeader'
 import { notFound } from 'next/navigation'
-
-// TODO: move to types/props folder
-interface GuessingProps {
-	params: {
-		gameId: string
-	}
-}
+import useClip from '@/hooks/useClip'
+import type { GuessingProps } from '@/types/props/GuessingProps'
 
 export default async function Guessing(props: GuessingProps) {
 	const gameId = props.params.gameId
 	if (gameId.length > 15) return notFound()
 	const { getClips } = useClip()
-	const { data: clips, error } = await getClips(gameId, 1)
+	const { data: clips, error } = await getClips(gameId, props.searchParams.page)
 	if (error) return notFound()
 	if (clips.length === 0) return Invalid({ gameId })
 	return (
 		<Section>
-			<YoutubeFrame
-				title='test'
-				url='https://www.youtube.com/embed/N8Oj9EnU72Q'
-			/>
+			<GuessingHeader clips={clips} />
+			{/*<GuessingContent />*/}
+			{/*<GuessingFooter />*/}
 		</Section>
 	)
 }
