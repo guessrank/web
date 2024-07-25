74d8baf92e38a21d8f6dde9d49940dddd201f042
Manu <mandrescabral@gmail.com>
2024-07-25 16:20:13 -0300

    feat: Add result state and functionality to useGuessing hook

--- src/app/guessing/[gameId]/hooks/useGuessing.ts
+++ src/app/guessing/[gameId]/hooks/useGuessing.ts
@@ -3,18 +3,25 @@ import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import useClipsStore from '@/store/clips'
 import useSelectionStore from '@/store/selection'
 import type { ClipType } from '@/types/ClipType'
+import type { ResultState } from '@/types/states/ResultState'
 
 const useGuessing = (listClips: ClipType[]) => {
 	const searchParams = useSearchParams()
 	const pathname = usePathname()
 	const { replace } = useRouter()
 
+	const [result, setResult] = useState<ResultState>({
+		message: 'Unknown',
+		status: false,
+		clipRef: {} as ClipType,
+	})
+
 	const clips = useClipsStore((state) => state.clips)
 	const setClips = useClipsStore((state) => state.setClips)
 	const remove = useClipsStore((state) => state.remove)
 	const [currentPage, setCurrentPage] = useState(1)
 	const [buttonEnabled, setButtonEnabled] = useState(false)
-	const { rankLevel } = useSelectionStore()
+	const { rankLevel, divisionLevel } = useSelectionStore()
 
 	useEffect(() => setClips(listClips), [])
 	useEffect(() => {
@@ -24,25 +31,37 @@ const useGuessing = (listClips: ClipType[]) => {
 		}
 	}, [clips])
 
+	const onLoadYoutubeFrame = () => setButtonEnabled(true)
 	const checkPageStatus = () => {
 		const params = new URLSearchParams(searchParams)
 		params.set('page', currentPage.toString())
 		const newUrl = `${pathname}?${params.toString()}`
 		replace(newUrl)
 	}
-
-	const nextClip = () => {
+	const closeResult = (resultFrame: React.RefObject<HTMLDivElement>) => () => {
+		resultFrame.current?.classList.add('hidden')
+		resultFrame.current?.classList.remove('flex')
+	}
+	const nextClip = (resultFrame: React.RefObject<HTMLDivElement>) => () => {
+		resultFrame.current?.classList.remove('hidden')
+		resultFrame.current?.classList.add('flex')
+		if (
+			rankLevel === clips[0].rankLevel &&
+			divisionLevel === clips[0].divisionLevel
+		)
+			setResult({ message: 'Correct', status: true, clipRef: clips[0] })
+		else setResult({ message: 'Incorrect', status: false, clipRef: clips[0] })
 		if (rankLevel === 0) return
 		setButtonEnabled(false)
 		remove()
 	}
-	const onLoadYoutubeFrame = () => setButtonEnabled(true)
-
 	return {
 		clips,
+		result,
 		buttonEnabled,
 		onLoadYoutubeFrame,
 		nextClip,
+		closeResult,
 	}
 }
 
