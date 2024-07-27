0b2326d7fb87be2a22e4bf158c201944c8667cb9
Manu <mandrescabral@gmail.com>
2024-07-27 15:28:50 -0300

    feat: add open Graph meta tags to improve social sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import '../styles/globals.css'
 import { staticMetadata } from '@/utils/siteStaticMetadata'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Header from '@/components/Header'
+import Head from 'next/head'
 
 export const metadata: Metadata = staticMetadata
 
@@ -13,6 +14,32 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
+			<Head>
+				<meta
+					property='og:image'
+					content='https://guessrank.xyz/images/logo.svg'
+				/>
+				<meta
+					property='og:site_name'
+					content='GuessRank.xyz'
+				/>
+				<meta
+					property='og:title'
+					content='GuessRank'
+				/>
+				<meta
+					property='og:type'
+					content='website'
+				/>
+				<meta
+					property='og:url'
+					content='https://guessrank.xyz/'
+				/>
+				<meta
+					property='og:description'
+					content='Guess the Rank of Players in Games like Valorant, Rocket League, and more!'
+				/>
+			</Head>
 			<body className='bg-dark text-white'>
 				<SpeedInsights />
 				<Header />
