1d706aa47c02c73182973bcd68a8997498349cc3
Manu <mandrescabral@gmail.com>
2024-07-28 16:14:07 -0300

    refactor: update next.config.mjs with additional remotePatterns for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'http',
-                hostname: 'localhost',
-            }
-        ],
-    }
-};
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'http',
+				hostname: 'localhost',
+			},
+			{
+				protocol: 'https',
+				hostname: 'api.guessrank.xyz',
+			},
+		],
+	},
+}
 
-export default nextConfig;
+export default nextConfig
